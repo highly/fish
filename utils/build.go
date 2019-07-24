24ec0ea34678deef59751c660359e7b4a51f81bc
shawn <shawn0828@hotmail.com>
2019-07-24 20:25:48 +0800

    improve log output

--- utils/build.go
+++ utils/build.go
@@ -3,17 +3,28 @@ package utils
 import (
 	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"os/signal"
 	"path/filepath"
+	"strings"
 	"syscall"
 )
 
 const BinFIle = "localTmpMain"
 
+var hiddenLog = []string{"grpc/pickfirst.go", "resolver_conn_wrapper", "grpc/clientconn.go", "gateway_server.go", "tracing/config.go"}
+
 var ctx context.Context
 var cancel context.CancelFunc
+var outputChan chan logMsg
+
+type logMsg struct {
+	Type, Message, Message2 string
+}
 
 func GoBuildAndRun(location string) {
 	if GoBuild(location) {
@@ -40,6 +51,7 @@ func GoRun(location string) {
 	// stop program if already exists
 	if cancel != nil {
 		Common.Println("closing previous program...")
+		close(outputChan)
 		cancel()
 	}
 	TwoColorPrintLn("running new program => ", GetProgramName(location))
@@ -47,9 +59,14 @@ func GoRun(location string) {
 	ctx, cancel = context.WithCancel(context.Background())
 	cmd := exec.CommandContext(ctx, filepath.Join(location, BinFIle))
 	cmd.SysProcAttr = &syscall.SysProcAttr{Foreground: false}
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	stdout, _ := cmd.StdoutPipe()
+	stderr, _ := cmd.StderrPipe()
 	cmd.Start()
+
+	outputChan = make(chan logMsg, 1024)
+	go OutputStdOut(stdout)
+	go OutputStdErr(stderr)
+	go Output()
 }
 
 func Clean() {
@@ -64,5 +81,82 @@ func Clean() {
 		DelFile(filepath.Join(CurrentPath, BinFIle))
 		Red.Println("exit from fish.")
 		os.Exit(1)
+	}
+}
+
+type outputLog struct {
+	Level   string      `json:"level"`
+	Message string      `json:"message"`
+	Context interface{} `json:"context"`
+}
+
+func OutputStdErr(stderr io.ReadCloser) {
+	for {
+		buf := make([]byte, 10240)
+		logLen, err := stderr.Read(buf)
+		if err != nil {
+			return
+		}
+		if logLen <= 0 {
+			break
+		}
+		outContent := string(buf[:logLen])
+		show := true
+		for _, banned := range hiddenLog {
+			if strings.Contains(outContent, banned) {
+				show = false
+				break
+			}
+		}
+		if !show {
+			continue
+		}
+		var logContent outputLog
+		err = json.Unmarshal(buf[:logLen], &logContent)
+		if err != nil {
+			outputChan <- logMsg{
+				Type:    "stdErr_ori",
+				Message: outContent,
+			}
+		} else {
+			context := fmt.Sprintf("%v", logContent.Context)
+			if context != "map[]" {
+				logContent.Message = logContent.Message + "\n" + context
+			}
+			outputChan <- logMsg{
+				Type:     "stdErr_parsed",
+				Message:  logContent.Level + ": ",
+				Message2: logContent.Message,
+			}
+		}
+	}
+}
+
+func OutputStdOut(stdout io.ReadCloser) {
+	for {
+		buf := make([]byte, 10240)
+		logLen, err := stdout.Read(buf)
+		if err != nil {
+			return
+		}
+		if logLen <= 0 {
+			break
+		}
+		outputChan <- logMsg{
+			Type:    "stdOut",
+			Message: string(buf[:logLen]),
+		}
+	}
+}
+
+func Output() {
+	for msg := range outputChan {
+		if msg.Type == "stdErr_ori" {
+			FgMagenta.Printf("%s\n", msg.Message)
+		} else if msg.Type == "stdErr_parsed" {
+			WhiteAndFgMagenta(msg.Message, msg.Message2)
+		} else {
+			Yellow.Printf("\n%s\n", msg.Message)
+		}
 	}
 }
--- utils/print.go
+++ utils/print.go
@@ -1,16 +1,25 @@
 package utils
 
-import "github.com/fatih/color"
+import (
+	"github.com/fatih/color"
+	"log"
+)
 
-var Common, Red, Yellow *color.Color
+var Common, Red, Yellow, FgMagenta *color.Color
 
 func init() {
 	Common = color.New(color.FgCyan)
 	Red = color.New(color.FgRed)
 	Yellow = color.New(color.FgGreen).Add(color.Bold)
+	FgMagenta = color.New(color.FgMagenta)
 }
 
 func TwoColorPrintLn(common, yellow string) {
 	Common.Print(common)
 	Yellow.Println(yellow)
+}
+
+func WhiteAndFgMagenta(white, fgMagenta string) {
+	log.Print(white)
+	FgMagenta.Println(fgMagenta)
 }
